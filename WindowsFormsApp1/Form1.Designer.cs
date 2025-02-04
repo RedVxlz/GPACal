9099e2f03c872839f1e8258929612e9af144e6a6
RedVxlz <palawhat@gmail.com>
2025-02-04 22:03:49 +0700

    Update Form1.Designer.cs

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -74,9 +74,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(34, 100);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.Size = new System.Drawing.Size(75, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "ใส่คะแนน GPAX";
+            this.label2.Text = "คะแนน GPAX";
             // 
             // tbGPAx
             // 
